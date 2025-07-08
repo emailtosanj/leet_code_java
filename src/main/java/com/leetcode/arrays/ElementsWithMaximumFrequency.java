8a29ae115732bde16ec5417d2ca4298c83955e14
Sanjay <sanjaygverma@gmail.com>
2025-07-08 09:39:08 +0800

    08Jul2025: efficient run time complexity improved to 1 ms

--- src/main/java/com/leetcode/arrays/ElementsWithMaximumFrequency.java
+++ src/main/java/com/leetcode/arrays/ElementsWithMaximumFrequency.java
@@ -5,28 +5,23 @@ import java.util.Arrays;
 public class ElementsWithMaximumFrequency {
     public int maxFrequencyElements(int[] nums) {
 
+        int maxFreq = 0;
         int[] frq = new int[101];
-        for(int num : nums)
-        {
+        for (int num : nums) {
             frq[num]++;
+            maxFreq = Math.max(maxFreq, frq[num]);
+
         }
 
-
-        int[] cp_freq = Arrays.copyOf(frq, frq.length);
-
-
-        Arrays.sort(cp_freq);
-        int frequency = cp_freq[cp_freq.length-1];
         int sum = 0;
-        for(int num : nums)
-        {
-            if(frq[num] == frequency)
-            {
-                sum ++;
+        for (int num : nums) {
+            if (frq[num] == maxFreq) {
+                sum++;
             }
         }
 
         return sum;
     }
+
 
 }
